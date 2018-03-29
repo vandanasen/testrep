5fb606644ed2b489fcfd16006d2837177631bc73
vandanasen <vandy_senthil@yahoo.com>
2018-03-29 11:35:19 -0400

    Add files via upload

---
+++ testweb2.py
@@ -0,0 +1,16 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        message = "Good day to you all"
+        self.wfile.write(bytes(message, "utf8"))
+        return
+def run():
+    print('starting server...')
+    server_address = ('127.0.0.1', 8080)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+run()
